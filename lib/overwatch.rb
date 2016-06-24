1f75397338478cd411becd5693129c3f52e16d6f
camdenbrown <camden.brown@metova.com>
2016-06-24 09:27:12 -0500

    implement Overwatch scraper

--- lib/overwatch.rb
+++ lib/overwatch.rb
@@ -3,8 +3,25 @@ require "httparty"
 
 module Overwatch
   include HTTParty
+  base_uri 'https://api.lootbox.eu'
 
-  def self.fetch_profile
+  def self.fetch_profile(system, region, battle_net_tag)
+    get("/#{system}/#{region}/#{battle_net_tag}/profile").parsed_response["data"]
+  end
 
+  def self.fetch_hero(system, region, battle_net_tag, hero_name)
+    get("/#{system}/#{region}/#{battle_net_tag}/hero/#{hero_name}/").parsed_response
+  end
+
+  def self.fetch_cumulative_hero_data(system, region, battle_net_tag)
+    get("/#{system}/#{region}/#{battle_net_tag}/allHeroes/").parsed_response
+  end
+
+  def self.fetch_achievements(system, region, battle_net_tag)
+    JSON.parse(get("/#{system}/#{region}/#{battle_net_tag}/achievements"))
+  end
+
+  def self.fetch_patch_notes
+    get('/patch_notes').parsed_response
   end
 end
