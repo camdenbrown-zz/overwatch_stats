d36803c3c8c1455ea8bdf2ee308150e3bffd1c31
camdenbrown <camden.brown@metova.com>
2016-06-24 15:11:14 -0500

    implement specs

--- spec/overwatch_spec.rb
+++ spec/overwatch_spec.rb
@@ -5,7 +5,33 @@ describe Overwatch do
     expect(Overwatch::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe '.fetch_profile' do
+    it 'returns the user profile' do
+      expect(Overwatch.fetch_profile('pc', 'us', 'Hollowman-1205')["username"]).to eq("Hollowman")
+    end
+  end
+
+  describe '.fetch_hero' do
+    it "returns a user's hero stats" do
+      expect(Overwatch.fetch_hero('pc', 'us', 'Hollowman-1205', 'Soldier76')).to include("HelixRocketsKills-MostinGame")
+    end
+  end
+
+  describe '.fetch_cumulative_hero_data' do
+    it "returns a user's cumulative hero stats" do
+      expect(Overwatch.fetch_cumulative_hero_data('pc', 'us', 'Hollowman-1205')).to include("MeleeFinalBlows")
+    end
+  end
+
+  describe '.fetch_achievements' do
+    it "returns a user's cumulative hero stats" do
+      expect(Overwatch.fetch_achievements('pc', 'us', 'Hollowman-1205')).to include("finishedAchievements")
+    end
+  end
+
+  describe '.fetch_achievements' do
+    it "returns a user's cumulative hero stats" do
+      expect(Overwatch.fetch_patch_notes[0]).to include("title")
+    end
   end
 end
